16929b40fa8faa785a84e9017f2ede59e13d6dd8
Megan Sharon <615329+mgnsharon@users.noreply.github.com>
2024-02-22 02:21:33 -0500

    intial app

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,23 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
-    ui.on_request_increase_value({
+    ui.on_calculate_gki({
         let ui_handle = ui.as_weak();
         move || {
             let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
+            let g_reading: f32 = match ui.get_g_reading().trim().parse() {
+                Ok(v) => match ui.get_unit_type().as_str() {
+                    "mg/dL" => v / 18.0,
+                    _ => v,
+                },
+                Err(_) => 0.0,
+            };
+            let k_reading: f32 = ui.get_k_reading().trim().parse().unwrap_or(0.0);
+            if g_reading > 0.0 && k_reading > 0.0 {
+                ui.set_gki_result(format!("{:.2}", g_reading / k_reading).into())
+            } else {
+                ui.set_gki_result("Please enter valid numbers.".into())
+            }
         }
     });
 
